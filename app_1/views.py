070b484100515cc60a91b6fbfd8bf8333a0bb2aa
uppalapatinandini <uppalapatinandini16@gmail.com>
2026-01-12 11:31:24 +0530

    Initial commit

---
+++ app_1/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def service(request):
+    return render(request, 'service.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def careers(request):
+    return render(request, 'careers.html')
