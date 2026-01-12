f09842897ad7339f1d8dec8b58ced07d12e47877
uppalapatinandini <uppalapatinandini16@gmail.com>
2026-01-12 11:52:37 +0530

    iam adding some code

--- app_1/urls.py
+++ app_1/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('/',views.home,name='home'),
-     path('',views.home,name='home'),
+     
     path('about/',views.about,name='about'),
     path('service/',views.service,name='service'),
     path('contact/',views.contact,name='contact'),
